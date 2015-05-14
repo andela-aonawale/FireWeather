da6b9bbfdb8c95321f0fc58b681adf0e0648a478
ahmed onawale <ahmed.onawale@andela.co>
2015-05-14 23:51:58 +0100

    re-design the UI and applied layout constrainst

--- FireWeather/WeatherViewController.swift
+++ FireWeather/WeatherViewController.swift
@@ -8,30 +8,17 @@
 
 import UIKit
 
-class WeatherViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
-
-    @IBOutlet weak var tableView: UITableView!
+class WeatherViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        tableView.contentInset = UIEdgeInsetsMake(-60, 0, 0, 0)
+        //tableView.contentInset = UIEdgeInsetsMake(-60, 0, 0, 0)
         // Do any additional setup after loading the view.
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
-    }
-    
-    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 4
-    }
-    
-    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCellWithIdentifier("Cell") as! UITableViewCell
-        var label: UILabel = cell.viewWithTag(100) as! UILabel
-        label.text = "hvjhhj"
-        return cell
     }
 
 }
